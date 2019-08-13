b9f765cfa2539033885d10b178dd4d49a613bd09
zjjc123 <zjjc123@hotmail.com>
2019-08-12 23:43:01 -0700

    Basic react layout

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// loading App.js to index.html
+ReactDOM.render(<App />, document.getElementById('root'));
